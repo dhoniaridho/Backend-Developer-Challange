90abbcac54a6d49e0925931a8d0d01506b10ff77
dhoniaridho <dhoniaridho@gmail.com>
2023-05-13 08:14:41 +0700

    feat: add interceptor response

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppModule as AppBaseModule } from './app';
 import { ConfigModule } from './config';
+import { TransformInterceptor } from './common';
 
 @Module({
     imports: [AppBaseModule, ConfigModule],
+    providers: [
+        {
+            provide: 'APP_INTERCEPTOR',
+            useClass: TransformInterceptor,
+        },
+    ],
 })
 export class AppModule {}
